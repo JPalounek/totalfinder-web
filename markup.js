e17c6a3ee45372183b560f54a185915a2d4d9d3c
Jan Palounek <jan@palounek.com>
2012-08-11 23:07:01 +0200

    Revert.

--- markup.js
+++ markup.js
@@ -4,19 +4,17 @@
  * Options
  *  -d - directory - Input directory
  *  -r - recursive - Walk through directory tree
- *  -c - contentscript - Content script raw url
  *  -h, --help - Display help
  */
 
 var fs = require('fs');
-var exec = require('child_process').exec;
+var processed = '';
 var args = process.argv.splice(2);
 
 // Default params
 var options = {
 	dir: './',
-	recursive: true,
-	content_script: ''
+	recursive: true
 }
 
 function displayHelp() {
@@ -24,7 +22,6 @@ function displayHelp() {
  	console.log('by Jan Palounek 2012, binaryage.com');
  	console.log('Options:');
  	console.log(' -d - directory - Input directory');
- 	console.log(' -c - contentscript - Content script raw url');
  	// console.log(' -r - recursive - Walk through directory tree');
  	console.log(' -h, --help - Display help');
 }
@@ -52,16 +49,13 @@ function walkThrough(dir, done) {
   });
 };
 
-for(var i in args) {
+for (var i in args) {
 	switch(args[i]) {
 		case '-d':
 			options.dir = args[parseInt(i) + 1];
 			break;
 		case '-r':
 			options.recursive = true;
-			break;
-		case '-c':
-			options.content_script = args[parseInt(i) + 1];
 			break;
 		case '-h':
 			displayHelp();
@@ -73,11 +67,8 @@ if(args.length == 0) {
 	displayHelp();
 }
 
-if(options.content_script == '') {
-	console.log('!! Error - Content script is required !!')
-} else {
-	options.content_script = '<script type="instaedit/contentscript" src="' + options.content_script + '"></script>';
-}
+// Filter files from input dir
+console.log('Will markup: ');
 
 function filterMds(dir, mds) {
 	var markupable = [];
@@ -92,7 +83,7 @@ function filterMds(dir, mds) {
 	});
 }
 
-function replaceIn(repo, markupable, cb) {
+function replaceIn(markupable, cb) {
 	var replaced = {};
 	for(var i in markupable) {
 		file = fs.readFileSync(markupable[i]).toString();
@@ -100,10 +91,9 @@ function replaceIn(repo, markupable, cb) {
 		var source = file;
 			
 		file = file.split('---\n');
-		//https://raw.github.com/JPalounek/totalfinder-web/gh-pages/licensing.md
-		//https://raw.github.com/JPalounek/totalfinder-web.git/licensing.md
+
 		var content = file[2];
-		var marked = '<span data-content-origin="' + repo + markupable[i].replace(options.dir, '') + '">' + file[2] + '</span>';
+		var marked = '<span data-content-origin="' + markupable[i].replace(options.dir, '') + '">' + file[2] + '</span>';
 
 		replaced[markupable[i]] = source.replace(content, marked);
 	}
@@ -111,38 +101,12 @@ function replaceIn(repo, markupable, cb) {
 	cb(replaced);
 }
 
-function getBranch(cons) {
-	cons = cons.split('\n');
-	for (var i in cons) {
-		if(cons[i][0] == '*') {
-			return cons[0].split(' ')[1];
-		}
-	}
-}
-
-// Filter files from input dir
-console.log('Will markup: ');
-
 filterMds(options.dir, function(mds) {
 	// Iterate and markup
-	exec('git remote -v', function (error, stdout, stderr) {
-		exec('git branch -v', function (branchError, branchStdout, branchStderr) {
-			var branch = getBranch(branchStdout);
-
-			repo = stdout.split('origin	')[1].split(' (fetch)')[0].split(' ').join('');
-			repo = repo.replace('.git', '')
-			repo = repo.replace('https://github.com/', 'https://raw.github.com/');
-			repo = repo + '/' + branch;
-	
-			replaceIn(repo, mds, function (replaced) {
-				for(var i in replaced) {
-					fs.writeFile(/*'_site/' + */i, replaced[i] + options.content_script, function (err) {
-  						if (err) throw err;
-					});
-				}
-			});
-		});
+	replaceIn(mds, function (replaced) {
+		console.log('-');
+		for(var i in replaced) {
+			console.log(replaced[i]);
+		}
 	});
 });
-
-console.log('done');
